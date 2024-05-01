51c71fd9fb8ad73c356d9ebf16cb63a283990dfd
MTayyab4 <126855912+MTayyab4@users.noreply.github.com>
2024-05-02 01:28:12 +0800

    Add files via upload

---
+++ image-list.js
@@ -0,0 +1,29 @@
+export const Path = 'gallery/';
+
+export const Gallery = {
+  images: [
+    {
+      name: 'Darth Vader',
+      alt: 'A Black Clad warrior lego toy',
+      url: 'gallery/cache2.png',
+      credit:
+        '<a href="https://www.flickr.com/photos/legofenris/">legOfenris</a>, published under a <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">Attribution-NonCommercial-NoDerivs 2.0 Generic</a> license.',
+    },
+
+    {
+      name: 'Snow Troopers',
+      alt: 'Two lego solders in white outfits walking across an icy plain',
+      url: 'gallery/order.png',
+      credit:
+        '<a href="https://www.flickr.com/photos/legofenris/">legOfenris</a>, published under a <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">Attribution-NonCommercial-NoDerivs 2.0 Generic</a> license.',
+    },
+
+    {
+      name: 'Bounty Hunters',
+      alt: 'A group of bounty hunters meeting, aliens and humans in costumes.',
+      url: 'gallery/WhatsApp Image 2024-04-29 at 9.41.35 PM.jpeg',
+      credit:
+        '<a href="https://www.flickr.com/photos/legofenris/">legOfenris</a>, published under a <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">Attribution-NonCommercial-NoDerivs 2.0 Generic</a> license.',
+    },
+  ],
+};
